8c777f349ae717f4f757907ea56314b72e08baaa
André Moura <andre.benedicto@etec.sp.gov.br>
2020-05-03 01:20:55 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+$(document).ready(() => {
+
+	$('button').on('click', e => {
+		e.preventDefault()
+
+		let dados = $('form').serialize()
+		//console.log(dados)
+
+		$.ajax({
+			type: 'post',
+			url: 'app.php',
+			data: dados,
+			dataType: 'json',
+			success: dados => {
+				console.log(dados)
+			},
+			error: erro => {
+				console.log(erro)
+			}
+		})
+	})
+
+})
